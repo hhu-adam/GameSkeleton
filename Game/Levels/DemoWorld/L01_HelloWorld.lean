9634e2c12f1ee67844e2971b796a269e8bfa106e
joneugster <eugster.jon@gmail.com>
2023-11-30 10:13:54 +0100

    complete devcontainer setup

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -13,10 +13,10 @@ Statement (h : x = 2) (g: y = 4) : x + x = y := by
   Hint "You can either start using `h` or `g`."
   Branch
     rw [g]
-    Hint "You should use {h} now."
+    Hint "You should use `{h}` now."
     rw [h]
   rw [h]
-  Hint "You should use {g} now."
+  Hint "You should use `{g}` now."
   rw [g]
 
 Conclusion "This last message appears if the level is solved."
