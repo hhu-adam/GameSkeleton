e96e1cf150cd49ae5de94c4021dad2ba20117b13
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-14 22:39:25 +0100

    remove mention of NewLemma in comment
    I'm just re-learning how to use the game software, and this deprecated comment just caught me out :-)

--- Game/Levels/DemoWorld/L01_HelloWorld.lean
+++ Game/Levels/DemoWorld/L01_HelloWorld.lean
@@ -24,5 +24,5 @@ Conclusion "This last message appears if the level is solved."
 /- Use these commands to add items to the game's inventory. -/
 
 NewTactic rw rfl
--- NewLemma Nat.add_comm Nat.add_assoc
+-- NewTheorem Nat.add_comm Nat.add_assoc
 -- NewDefinition Nat Add Eq
