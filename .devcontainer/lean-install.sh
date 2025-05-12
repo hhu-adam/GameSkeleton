661ad1f393c0d9891490eda02028e3f1d6568f3e
Steven Clontz <steven.clontz@gmail.com>
2025-05-12 14:48:28 -0500

    remove docker files, start troubleshooting lean-install

--- .devcontainer/lean-install.sh
+++ .devcontainer/lean-install.sh
@@ -1,4 +1,4 @@
 export LEAN_VERSION="$(cat lean-toolchain)"
-curl https://elan.lean-lang.org/elan-init.sh -sSf | sh -s -- -y --default-toolchain $LEAN_VERSION;
-lake update -R
-lake build
+# curl https://elan.lean-lang.org/elan-init.sh -sSf | sh -s -- -y --default-toolchain $LEAN_VERSION;
+# lake update -R
+# lake build
