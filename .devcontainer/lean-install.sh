0ceffba43a4f4568d6af4b70c4a28242bb4f00f8
Steven Clontz <steven.clontz@gmail.com>
2025-05-12 14:40:04 -0500

    Update .devcontainer/lean-install.sh

--- .devcontainer/lean-install.sh
+++ .devcontainer/lean-install.sh
@@ -1,4 +1,4 @@
 export LEAN_VERSION="$(cat lean-toolchain)"
-curl curl https://elan.lean-lang.org/elan-init.sh -sSf | sh -s -- -y --no-modify-path --default-toolchain $LEAN_VERSION;
+curl https://elan.lean-lang.org/elan-init.sh -sSf | sh -s -- -y --default-toolchain $LEAN_VERSION;
 lake update -R
 lake build
