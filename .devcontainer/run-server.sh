49142fb0f6cdd00af24abf2cfdb8f7c8be79b433
Steven Clontz <steven.clontz@gmail.com>
2025-05-16 10:35:19 -0500

    pop up correct page after server runs

--- .devcontainer/run-server.sh
+++ .devcontainer/run-server.sh
@@ -1,3 +1,4 @@
 cd ../lean4game
 export VITE_LEAN4GAME_SINGLE=true
+(sleep 3 && python -m webbrowser -t "https://${CODESPACE_NAME}-3000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/#/g/local/$(basename $GITHUB_REPOSITORY)/")&
 npm start
