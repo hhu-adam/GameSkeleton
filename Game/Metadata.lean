f6e2b36a2bcd7334ee55c8026520b87a91365146
Jon Eugster <eugster.jon@gmail.com>
2025-08-09 01:19:21 +0200

    bump lean4game

--- Game/Metadata.lean
+++ Game/Metadata.lean
@@ -1,6 +1,4 @@
-import GameServer.Commands
-import Mathlib.Tactic.Linter
-
+import GameServer
 -- import Mathlib.Tactic.Common
 
 /-! Use this file to add things that should be available in all levels.
@@ -12,6 +10,6 @@ you are completely free how you structure your lean project, this is merely
 a suggestion.
 
 *Bug*: However, things are bugged out if the levels of different worlds are imported
-in a random order. Therefore, you should keep the structure of one file lean file per world
-that imports all its levels.
+in a random order. Therefore, you should keep the structure of one file Lean file per world
+which imports all its levels.
 -/
