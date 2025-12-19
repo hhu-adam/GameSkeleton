f2505c4ba4ae8cd5aee3caa397a51ff0fb271a5b
Success Moses <msucc2024@gmail.com>
2025-12-19 10:08:56 +0100

    update

--- .devcontainer/run-server.sh
+++
@@ -1,5 +0,0 @@
-export VITE_LEAN4GAME_SINGLE=true
-export VITE_LEAN4GAME_SINGLE_NAME=$(basename "$PWD")
-cd ../lean4game
-(sleep 3 && python -m webbrowser -t "https://${CODESPACE_NAME}-3000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}")&
-npm start
